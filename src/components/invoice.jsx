58c528acf6d6050bd4157816f696d0b57d975c13
Afshin <123559664+HgmnDV03@users.noreply.github.com>
2023-02-02 22:36:55 +0330

    show info of selected invoices

--- src/components/invoice.jsx
+++ src/components/invoice.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import {useParams} from 'react-router-dom';
+import {getInvoice} from '../data';
 
 function Invoice() {
     let params = useParams();
+    let invoice = getInvoice(parseInt(params.invoiceId, 10));
     return (
-        <h2>Invoice: {params.invoiceId}</h2>
+        <main style={{padding:'1rem'}}>
+            <h2>Total due: {invoice.amount}</h2>
+            <p>{invoice.name}: {invoice.number}</p>
+            <p>Due Date: {invoice.due}</p>
+        </main>
     );
 }
 
