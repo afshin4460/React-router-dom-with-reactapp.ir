6d10b5f30ca2c9eabc742b21271e07c4e64be20b
Afshin <123559664+HgmnDV03@users.noreply.github.com>
2023-02-02 20:03:21 +0330

    invoices added

--- src/components/invoices.jsx
+++ src/components/invoices.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+import {getInvoices} from '../data';
 
 function Invoices() {
+    let invoices = getInvoices();
     return (
-        <>
-            <h1>Invoices</h1>
-        </>
+        <div style={{display: 'flex'}}>
+            <nav style={{borderRight: '1px solid', padding: '1rem'}}>
+                {
+                    invoices.map(invoice => (
+                        <Link key={invoice.number} to={`/invoices/${invoice.number}`} style={{display: 'block', margin: '1rem 0'}}>
+                            { invoice.name }
+                        </Link>
+                    ))
+                }
+            </nav>
+        </div>
     );
 }
 
