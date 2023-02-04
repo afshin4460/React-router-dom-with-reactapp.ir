c0f41f0fac42cdb0f30c7aff64a5aab271954109
Afshin <123559664+HgmnDV03@users.noreply.github.com>
2023-02-05 00:33:18 +0330

    use set params

--- src/components/invoices.jsx
+++ src/components/invoices.jsx
@@ -1,15 +1,32 @@
 import React from 'react';
-import {NavLink, Outlet} from 'react-router-dom';
+import {NavLink, Outlet, useSearchParams} from 'react-router-dom';
 import {getInvoices} from '../data';
 import './invoices.css';
 
 function Invoices() {
     let invoices = getInvoices();
+    const [searchParams, setSearchParams] = useSearchParams();
+
+    const handleChange = (event) => {
+        let filter = event.target.value;
+        if(filter) {
+            setSearchParams({filter});
+        } else {
+            setSearchParams({});
+        }
+    };
+
     return (
         <div style={{display: 'flex'}}>
             <nav style={{borderRight: '1px solid', padding: '1rem'}}>
+                <input type="text" value={searchParams.get('filter') || ''} onChange={handleChange} />
                 {
-                    invoices.map(invoice => (
+                    invoices.filter(invoice => {
+                        let filter = searchParams.get('filter');
+                        if (!filter) return true;
+                        let name = invoice.name.toLowerCase();
+                        return name.startsWith(filter.toLowerCase());
+                    }).map(invoice => (
                         <NavLink key={invoice.number} to={`/invoices/${invoice.number}`} className={
                             ({isActive}) => isActive ? 'link red' : 'link blue'
                         }>
